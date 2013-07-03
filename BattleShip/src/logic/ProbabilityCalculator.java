2dae7c4af47fe7193e170a62885845b9621dedfc
Justin Nelson <justin.john.nelson@gmail.com>
2013-07-02 20:01:30 -0500

    Hashing out the probability calculator

--- BattleShip/src/logic/ProbabilityCalculator.java
+++ BattleShip/src/logic/ProbabilityCalculator.java
@@ -6,13 +6,24 @@ public class ProbabilityCalculator {
 
     private ShipGrid grid;
 
+    private int[][] canPlaceShip;
+
     public ProbabilityCalculator(ShipGrid grid) {
         this.grid = grid;
+        canPlaceShip = new int[grid.getWidth()][grid.getWidth()];
     }
 
     public void setProbabilities() {
         List<Ship> remainingShips = grid.remainingShips();
-        // not sure what to do here...
+        for (Ship ship : remainingShips) {
+            for (int i = 0; i < grid.getWidth(); i++) {
+                for (int j = 0; j < grid.getWidth(); j++) {
+                    for (int k = 0; k < ship.length; k++){
+                        boolean canPlace = true;
+                        GridSpace current = grid.get(i, j);
+                    }
+                }
+            }
+        }
     }
-
 }
